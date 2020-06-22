638afa1079047e52f62400995c4ca5e938de61cf
hyyc116 <hyyc116@gmail.com>
2020-06-22 20:03:36 +0800

    add

--- data_sampling.py
+++ data_sampling.py
@@ -39,5 +39,54 @@ def select_papers_by_subject(subjName,tag):
 	logging.info('data saved to {}.'.format(path))
 
 
+def GetCitRelationsInsubj(subjName,tag):
+
+	path = 'data/paper_ids_{}.txt'.format(tag)
+
+	paper_ids = set([line.strip() for line in open(path)])
+
+	outpath = 'data/pid_cits_{}.txt'.format(tag)
+
+	of = open(outpath,'w')
+
+	pid_citnum = defaultdict(int)
+
+	lines = []
+	for line in open('../cascade_temporal_analysis/data/pid_cits_ALL.txt'):
+
+        progress+=1
+
+        if progress%10000000==0:
+            logging.info('reading %d citation relations....' % progress)
+
+        line = line.strip()
+
+        pid,citing_id = line.split("\t")
+
+        if pid in paper_ids and citing_id in paper_ids:
+
+        	lines.append(line)
+
+        	pid_citnum[pid]+=1
+
+        if len(lines)>0 and len(lines)%100000==0:
+
+        	of.write('\n'.join(lines)+'\n')
+
+        	lines = []
+
+    if len(lines)>0 and len(lines)%100000==0:
+
+        	of.write('\n'.join(lines)+'\n')
+
+        	lines = []
+
+
+    of.close()
+
+    open('data/pid_citnum_{}.json'.format(tag),'w').write(json.dumps(pid_citnum))
+    logging.info('citation relations saved to {}, pid citnum saved to {}.'.format(outpath,'data/pid_citnum_{}.json'.format(tag)))
+
 if __name__ == '__main__':
 	select_papers_by_subject('computer science','cs')
+	GetCitRelationsInsubj('computer science','cs')
