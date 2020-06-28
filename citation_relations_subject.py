4744d0c21cbf35874370cebe700f11b7804805b7
hyyc116 <hyyc116@gmail.com>
2020-06-28 08:22:14 +0800

    add

--- citation_relations_subject.py
+++ citation_relations_subject.py
@@ -3,6 +3,11 @@
 根据不同给的subject标签对wos的论文进行分领域处理
 
 '''
+
+from basic_config import *
+
+
+
 '''
 
 选择6个top领域，然后在6个top领域内分别选择一个子领域作为实验数据。
@@ -15,7 +20,34 @@ def get_paperids_of_subjects(subjName):
 ##统计wos所有论文的citation count随着时间的变化情况
 def stats_citation_count_of_papers():
 
-	pass
+	logging.info('loading paper year obj ...')
+	paper_year = json.loads(open('../cascade_temporal_analysis/data/pubyear_ALL.json').read())
+
+	logging.info('start to stat citation relations ...')
+
+	pid_year_citnum = defaultdict(lambda:defaultdict(int))
+
+    progress = 0
+
+    lines = []
+    for line in open('../cascade_temporal_analysis/data/pid_cits_ALL.txt'):
+
+        progress+=1
+
+        if progress%10000000==0:
+            logging.info('reading %d citation relations....' % progress)
+
+        line = line.strip()
+
+        pid,citing_id = line.split("\t")
+
+        if paper_year.get(pid,None) is None or paper_year.get(citing_id,None) is None:
+        	continue
+
+        pid_year_citnum[pid][int(paper_year[citing_id])]+=1
+
+    open('data/pid_year_citnum.json','w').write(json.dumps(pid_year_citnum))
+    logging.info('pid year citnum saved to data/pid_year_citnum.json.')
 
 ##整体领域高被引论文的平均数随着数据规模的变化情况
 def general_top_citation_trend_over_datasize():
@@ -26,3 +58,8 @@ def general_top_citation_trend_over_datasize():
 def temporal_top_citation_trend_over_datasize():
 
 	pass
+
+
+if __name__ == '__main__':
+	## 统计论文引用次数随着时间的变化
+	stats_citation_count_of_papers()
